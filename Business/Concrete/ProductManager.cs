bea7a14484f5feb3837de48877d137430d415adf
Emir Burak Akkuş <emir.akkus@ogr.sakarya.edu.tr>
2025-06-13 12:02:35 +0300

    13.06.2025 8 Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,12 +1,6 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
-using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
@@ -24,6 +18,16 @@ namespace Business.Concrete
             //İş kodları burada olacak.
             //Yetkisi var mı
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetBuyUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=> p.UnitPrice>=min && p.UnitPrice<=max);
         }
     }
 }
